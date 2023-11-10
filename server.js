85a8bf1b558b352346807a5bb1ea22638c31acda
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-11-10 11:14:28 +0100

    update ex

--- server.js
+++ server.js
@@ -1,48 +1,43 @@
-const http = require('http');
-const dotenv = require("dotenv");
-// importo utilities
-const {loadJokes} = require('./utilities');
-// deve essere eseguito prima di accedere alle variabili d'ambiente
-dotenv.config();
+const http = require("http");
+const { sum, loadStringNorris, loadAjaxgNorris } = require("./utilities.js");
 
-const port = process.env.PORT || 8888;
+// Imposto la porta e l'host per il server
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
 
+// Definisco l'URL dell'API delle barzellette su Chuck Norris
+const chuckNorrisApiUrl = "https://api.chucknorris.io/jokes/random";
 
+// Carico le variabili d'ambiente da un file .env
+require('dotenv').config();
 
-// Creazione del server
-const server = http.createServer((req, res) => {
-// per richiamarla la callback function la passo come argomento
+// Importo il modulo fs per la gestione dei file
+const fs = require('fs');
 
-loadJokes((jokes)=>{
-    const html=[];
-        // return
-    html.push('<ul>');
-    // destructuring
-    for (const joke of jokes) {
-    
-        html.push(`<li>${joke.value}</li>`);
-    }
-    html.push('</ul>');
+// Creo il server HTTP
+http.createServer(function (req, res) {
+    // Imposto l'header per il tipo di contenuto della risposta
+    res.setHeader("Content-Type", "text/html;charset=utf-8");
 
-    // risultato nel web
-    res.setHeader('Content-Type','text/html;charset=utf-8');
-    res.end(html.join(''));
+    // Eseguo una richiesta alla Chuck Norris API per ottenere una barzelletta
+    fetch(chuckNorrisApiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Stampo la barzelletta nel formato HTML
+            res.end(`<h1>${data.value}</h1>`);
+
+            // Aggiungo la barzelletta al file norris.json
+            const jsonToAdd = JSON.stringify(data.value);
+
+            // Scrivo nel file
+            fs.appendFile("norris.json", jsonToAdd, function (err) {
+                if (err) throw err;
+                console.log("ok!");
+            });
+        });
+
+}).listen(port, host, () => {
+    // Mostro un messaggio di avvio del server
+    const serverUrl = `http://${host}:${port}`;
+    console.log(`Server avviato su ${serverUrl}`);
 });
-
- const html=[];
- return
-
-
- 
-// risultato nel web
-res.setHeader('Content-Type','text/html;charset=utf-8');
-res.end('hello wolrd');
-});
-
-
-
-// Avvio del server
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
-
--- utilities.js
+++ utilities.js
@@ -1,30 +1,52 @@
-const fetch = require("node-fetch").default;
 
-/**
- * 
- * @param {function} onSuccess 
- */
-// per stampare i dati abbiamo bisogno di una callback :onSucces in questo caso
-function loadJokes(onSuccess){
-const url = 'https://api.chucknorris.io/jokes/random';
-
-
-fetch(url)
-// decodifica il codice .json ritornano una promise quindi bisogna agg then in modo da ascoltare la risposta anche la risposta response.json()
-.then((response)=>response.json())
-.then((data)=>{
-   console.log(data);
-   onSuccess(data);
-
-});
-    
+function sum(num1, num2){
+    // Restituisco la somma dei due numeri
+    return num1 + num2;
 }
 
+// Importo i moduli necessari
+const fs = require('fs');
+const path = require("path");
 
+// Funzione per caricare le barzellette su Chuck Norris da un file JSON locale
+function loadStringNorris(){
 
+    // Definisco il percorso del file JSON locale
+    const pathData = path.join(__dirname, "norris.json");
 
+    // Leggo il file in modo sincrono
+    try {
+        // Leggo il contenuto del file come stringa
+        const norrisString = fs.readFileSync(pathData, "utf-8");
 
-// esporto
+        // Converto la stringa in un array di oggetti
+        return JSON.parse(norrisString);
+    } catch (err) {
+        // Gestisco gli errori, se presenti, durante la lettura del file
+        console.log(err.message);
+
+        // Restituisco un array vuoto in caso di errore
+        return [];
+    }
+}
+
+// Funzione per caricare le barzellette su Chuck Norris da un'API esterna tramite Ajax
+function loadAjaxgNorris(){
+    // Definisco l'URL dell'API delle barzellette su Chuck Norris
+    const url = "https://api.chucknorris.io/jokes/random";
+
+    // Effettuo una richiesta fetch all'API
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => 
+            // Estraggo e restituisco il valore della barzelletta dalla risposta dell'API
+            data.value
+        );
+}
+
+// Esporto le funzioni come parte del modulo
 module.exports = {
-    loadJokes
+    sum,
+    loadStringNorris,
+    loadAjaxgNorris
 };
